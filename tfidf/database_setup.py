bffc5feb792953666c85b3897f4b92102a2d50ea
Taras Bunyk <tbunyk@gmail.com>
2018-09-30 23:34:29 +0300

    Add daatabase query

--- tfidf/database_setup.py
+++ tfidf/database_setup.py
@@ -1,7 +1,10 @@
 from asyncpg import create_pool
 
+_pool = None
+
 async def register_db(app, loop):
-    app.pool = await create_pool(
+    global _pool
+    _pool = await create_pool(
         host=app.config.DB_HOST,
         user=app.config.DB_USER,
         password=app.config.DB_PASSWORD,
@@ -10,13 +13,16 @@ async def register_db(app, loop):
         loop=loop,
         max_size=100
     )
-    async with app.pool.acquire() as connection:
+    async with acquire_pool() as connection:
         await connection.execute("""
             CREATE TABLE IF NOT EXISTS pages (
             id serial primary key,
             title varchar(255) unique not null,
             text text
         );""")
+
+def acqire_pool():
+    return _pool.acquire()
 
 def attach_db(app):
     before_server_start = app.listener('before_server_start')
--- tfidf/page.py
+++ tfidf/page.py
@@ -1,11 +1,21 @@
 from sanic.views import HTTPMethodView
 from sanic.response import json
 
+from tfidf.database_setup import acquire_pool
+
 class PageView(HTTPMethodView): 
-    def get(self, request, title):
+    async def get(self, request, title):
         print("Getting", title)
+        async with acquire_pool() as connection:
+            results = await connection.fetch('SELECT title, text FROM pages WHERE title = $1', title)
+            print(results)
         return json({"hello": "world"})
 
-    def post(self, request, title):
-        print("posting", title)
-        return json({"hello": "world"})
+    async def post(self, request, title):
+        async with acquire_pool() as connection:
+            async with connection.transaction():
+                await connection.execute('''
+                    INSERT INTO pages (title, text) VALUES ($1, $2)
+                ''', title, request.json['text']
+                )
+                return json({"status": "success"})
